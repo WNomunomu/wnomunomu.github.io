726cbdeb09046533fa1c45654b0d50cdce3c3daf
WNomunomu <kohsei.nmr@gmail.com>
2023-11-26 01:08:17 +0900

    use laytout

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/components/base/Layout";
+import { Index } from "@/components/page/Index/Index";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Index />
+    </Layout>
+  );
+}
