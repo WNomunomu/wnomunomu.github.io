0e5bbf75fda69c6382e7474f0b30c8d2d97b75fe
WNomunomu <kohsei.nmr@gmail.com>
2023-12-19 17:55:08 +0900

    create my skill set

---
+++ src/components/page/Index/MySkillSet.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './indexPage.module.css'
+
+export const MySkillSet = () => {
+  return (
+    <div className="text-center vh-100 bg-body-secondary">
+      <div>My skill set</div>
+      <div>フロントエンド: react</div>
+    </div>
+  );
+};
