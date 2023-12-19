255efa3d263aa22cfd75f70377f98099c1cf3ab9
WNomunomu <kohsei.nmr@gmail.com>
2023-12-19 17:54:52 +0900

    create contact me

---
+++ src/components/page/Index/ContactMe.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '.indexPage.module.css'
+
+export const ContactMe = () => {
+  return (
+    <div className="text-center vh-100 bg-body">
+      <div>Contact me</div>
+      <div>連絡してね☆</div>
+    </div>
+  );
+};
