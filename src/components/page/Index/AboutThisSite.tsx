2e75617aeeed238499aaf14f7c64b4436d118974
WNomunomu <kohsei.nmr@gmail.com>
2023-12-19 17:54:40 +0900

    create about this site

---
+++ src/components/page/Index/AboutThisSite.tsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './indexPage.module.css'
+
+export const AboutThisSite = () => {
+  return (
+    <div className="text-center vh-100 bg-body d-flex align-items-center justify-content-center">
+      <div className='content'>
+        <div className="fs-1 fw-bold">About this site</div>
+        <div className="fs-5">
+          <div>ここはWNomunomuのポートフォリオサイトです。</div>
+          <div>ここはWNomunomuのポートフォリオサイトです。</div>
+          <div>ここはWNomunomuのポートフォリオサイトです。</div>
+        </div>
+      </div>
+    </div>
+  );
+};
