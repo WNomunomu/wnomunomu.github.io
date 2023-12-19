33bfe2fccfc018c9962bec290ebb1be2997bfcb0
WNomunomu <kohsei.nmr@gmail.com>
2023-12-19 17:57:16 +0900

    fix about me

--- src/components/page/Index/AboutMe.tsx
+++ src/components/page/Index/AboutMe.tsx
@@ -9,7 +9,7 @@ export const AboutMe = () => {
         <div className="fs-1 fw-bold">About me</div>
         <div className="fs-5">
           <div>群馬県出身のソフトウェアエンジニア。</div>
-          <div>中学、高校でプログラミングにハマり、現在は大学で機械工学を専攻しながら、エンジニアとして活動。</div>
+          <div>中学、高校でプログラミングにハマり、現在は大学で機械工学を専攻しながら、駆け出しエンジニアとして修業中。</div>
           <div>現在は主にフロントエンドの開発をしています。</div>
         </div>
         <div>
