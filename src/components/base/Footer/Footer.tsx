ebc691b7506564e5792b97217c9acac918af830b
WNomunomu <kohsei.nmr@gmail.com>
2023-12-29 23:08:16 +0900

    create footer

--- src/components/base/Footer/Footer.tsx
+++ src/components/base/Footer/Footer.tsx
@@ -1,5 +1,14 @@
 export const Footer = () => {
   return (
-    <h1>Footer</h1>
+    <>
+      <div className="text-light">
+        <div  className="fs-1 text-center p-5" style={{ backgroundColor: 'rgb(144, 202, 249)' }}>
+          <div>Thank you for coming!</div>
+        </div>
+        <div className="fs-6 p-3" style={{ backgroundColor: 'rgb(100, 181, 246)' }}>
+          <div className="mx-auto" style={{ width: '70%' }}>@2024 Kohsei Nomura</div>
+        </div>
+      </div>
+    </>
   );
 };
