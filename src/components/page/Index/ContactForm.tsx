9d00995aaf505cf0310bda02bc9ccdce927438f7
WNomunomu <kohsei.nmr@gmail.com>
2023-12-29 23:08:54 +0900

    create contact form

---
+++ src/components/page/Index/ContactForm.tsx
@@ -0,0 +1,43 @@
+import { FormEventHandler } from 'react';
+
+import styles from './indexPage.module.css'
+
+const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+  event.preventDefault();
+
+  const form = new FormData(event.currentTarget);
+
+  const name = form.get('name');
+  const email = form.get('email');
+  const comment = form.get('comment');
+
+  console.log(name);
+  console.log(email);
+  console.log(comment);
+};
+
+export const ContactForm = () => {
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <div className="row">
+            <div className="col-md-6">
+              <input type="name" className="form-control" placeholder="Name" name="name" />
+            </div>
+            <div className="col-md-6">
+              <input type="email" className="form-control" placeholder="Email" name="email" />
+            </div>
+          </div>
+        </div>
+        <div className="mb-3">
+          <textarea className="form-control" rows={3} placeholder="Comment" name="comment"></textarea>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button className={`${styles['button']} fs-5 text-light border-0 py-2 px-4 rounded`} type="submit">Send</button>
+        </div>
+      </form>
+    </>
+  );
+};
--- src/components/page/Index/ContactMe.tsx
+++ src/components/page/Index/ContactMe.tsx
@@ -1,11 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from '.indexPage.module.css'
+
+import { ContactForm } from './ContactForm';
 
 export const ContactMe = () => {
   return (
-    <div className="text-center vh-100 bg-body">
-      <div>Contact me</div>
-      <div>連絡してね☆</div>
+    <div className="vh-100 bg-body d-flex align-items-center justify-content-center">
+      <div className="mx-auto" style={{ width: '50%' }}>
+        <div className="fs-1 fw-bold text-center">Contact me</div>
+        <div className="fs-5 mb-4">
+          <div>最後までご覧いただきありがとうございました。このサイトを通して、私のことを少しでも知っていただけたのなら嬉しいです。もしこのサイトや私について何かコメントがありましたら、下記フォームをご利用ください。</div>
+        </div>
+        <ContactForm />
+      </div>
     </div>
   );
 };
