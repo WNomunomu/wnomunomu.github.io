eccc0dee9e01b9b5bda7cc9389646f8c215f2d54
WNomunomu <kohsei.nmr@gmail.com>
2023-12-29 23:09:57 +0900

    create myskillset component

--- src/components/page/Index/MySkillSet.tsx
+++ src/components/page/Index/MySkillSet.tsx
@@ -1,11 +1,138 @@
+import { FC } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './indexPage.module.css'
+
+type Props = {
+  title?: string
+  icon?: string
+}
+
+const skillSet = [
+  {
+    type: 'language',
+    title: 'HTML',
+    icon: 'devicon-html5-plain colored',
+  },
+  {
+    type: 'language',
+    title: 'CSS',
+    icon: 'devicon-css3-plain colored',
+  },
+  {
+    type: 'language',
+    title: 'JavaScript',
+    icon: 'devicon-javascript-plain colored',
+  },
+  {
+    type: 'language',
+    title: 'TypeScript',
+    icon: 'devicon-typescript-plain colored',
+  },
+  {
+    type: 'language',
+    title: 'Python',
+    icon: 'devicon-python-plain colored',
+  },
+  {
+    type: 'language',
+    title: 'MySQL',
+    icon: 'devicon-mysql-plain colored',
+  },
+  {
+    type: 'framework-library',
+    title: 'React',
+    icon: 'devicon-react-original colored'
+  },
+  {
+    type: 'framework-library',
+    title: 'Next.js',
+    icon: 'devicon-nextjs-original-wordmark colored'
+  },
+  {
+    type: 'framework-library',
+    title: 'express',
+    icon: 'devicon-express-original'
+  },
+  {
+    type: 'framework-library',
+    title: 'django',
+    icon: 'devicon-django-plain colored'
+  },
+  {
+    type: 'others',
+    title: 'Git',
+    icon: 'devicon-git-plain colored'
+  },
+  {
+    type: 'others',
+    title: 'Linux',
+    icon: 'devicon-linux-plain colored'
+  },
+  {
+    type: 'others',
+    title: 'Google Cloud',
+    icon: 'devicon-googlecloud-plain-wordmark colored'
+  },
+]
+
+const SkillCard: FC<Props> = (props) => {
+  const { title, icon } = props;
+
+  return (
+    <div className="col-2 p-3">
+      <i className={`${icon}`} style={{ fontSize: '70px' }} />
+      <div className="fs-5">{title}</div>
+    </div>
+  );
+
+};
 
 export const MySkillSet = () => {
   return (
-    <div className="text-center vh-100 bg-body-secondary">
-      <div>My skill set</div>
-      <div>フロントエンド: react</div>
+    <div className="text-center py-5 bg-body-secondary d-flex align-items-center justify-content-center">
+      <div className="mx-auto" style={{ width: '70%' }}>
+        <div className="fs-1 fw-bold">My skill set</div>
+        <div className="fs-5">実際に開発に使用したことのある技術をまとめてみました。</div>
+        <div className="pt-3">
+          <div className="fs-5">Language</div>
+          <div className="row d-flex justify-content-center">
+            {skillSet.map((skill, index) => (
+              skill.type === 'language' &&
+              <SkillCard
+                key={index}
+                title={skill.title}
+                icon={skill.icon}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="pt-3">
+          <div className="fs-5">Framework / Library</div>
+          <div className="row d-flex justify-content-center">
+            {skillSet.map((skill, index) => (
+              skill.type === 'framework-library' &&
+              <SkillCard
+                key={index}
+                title={skill.title}
+                icon={skill.icon}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="pt-3">
+          <div className="fs-5">Others</div>
+          <div className="row d-flex justify-content-center">
+            {skillSet.map((skill, index) => (
+              skill.type === 'others' &&
+              <SkillCard
+                key={index}
+                title={skill.title}
+                icon={skill.icon}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
