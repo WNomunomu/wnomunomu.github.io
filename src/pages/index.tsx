6c3b4b1ee7a144bf5f52b7f45d22688bf03330f3
WNomunomu <kohsei.nmr@gmail.com>
2023-12-29 23:10:14 +0900

    introduce devicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+import Head from "next/head";
 import { Layout } from "@/components/base/Layout";
 import { Index } from "@/components/page/Index/Index";
 
 export default function Home() {
   return (
-    <Layout>
-      <Index />
-    </Layout>
+    <>
+      <div>
+        <Head>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+        </Head>    
+      </div>
+      <Layout>
+        <Index />
+      </Layout>
+    </>
   );
 }
