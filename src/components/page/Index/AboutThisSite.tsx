01b3aac54deb266a3f494ab5bc47a975fd630503
WNomunomu <kohsei.nmr@gmail.com>
2023-12-29 23:08:42 +0900

    style aboutthissite

--- src/components/page/Index/AboutThisSite.tsx
+++ src/components/page/Index/AboutThisSite.tsx
@@ -3,13 +3,16 @@ import styles from './indexPage.module.css'
 
 export const AboutThisSite = () => {
   return (
-    <div className="text-center vh-100 bg-body d-flex align-items-center justify-content-center">
+    <div
+      className={`${styles['about-this-site']} text-center vh-100 bg-body d-flex align-items-center justify-content-center`}
+    >
       <div className='content'>
         <div className="fs-1 fw-bold">About this site</div>
         <div className="fs-5">
-          <div>ここはWNomunomuのポートフォリオサイトです。</div>
-          <div>ここはWNomunomuのポートフォリオサイトです。</div>
-          <div>ここはWNomunomuのポートフォリオサイトです。</div>
+          <div>ここはエンジニアNomuのポートフォリオサイトです。</div>
+          <div>これまでに手がけた制作物、身につけたスキルをまとめています。</div>
+          <div>あなたに、私のことをもっと深く知ってほしいと思って作りました。</div>
+          <div>どうぞお茶でも飲みながらごゆるりとサイト内を散策してください。</div>
         </div>
       </div>
     </div>
