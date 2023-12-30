56299ef3d796229c0a85bfd3535431a14a1dac50
WNomunomu <kohsei.nmr@gmail.com>
2023-12-30 13:05:28 +0900

    introduce google icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <div>
         <Head>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         </Head>    
       </div>
       <Layout>
